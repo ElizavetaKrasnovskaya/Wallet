74b72a8b157d294e8b4aa4e352eb262d7ae3dfd3
e.krasnovskaya <ElizavetaKrasnovskaya@gmail.com>
2022-05-14 10:12:39 +0300

    refactor: update api key

--- data/src/main/java/com/bsuir/data/const/Const.kt
+++ data/src/main/java/com/bsuir/data/const/Const.kt
@@ -1,6 +1,6 @@
 package com.bsuir.data.const
 
 const val SERVER = "https://matrix.org"
-const val APP_ID = "2066754437e536e2"
-const val AUTH_KEY = "cc2aa7a893a269aa904b7d30dcf84302bdaaaec8"
+const val APP_ID = "209607748bf21d9e"
+const val AUTH_KEY = "f34d4ea15ec089a792696e8fdaa921fae7da45d6"
 const val REGION = "us"
